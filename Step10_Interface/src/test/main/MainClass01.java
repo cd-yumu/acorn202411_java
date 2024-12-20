f7a3f866ed4e2fd1db70834d3430412bf636cce8
EugeneChoi <yoojin7526@naver.com>
2024-12-20 10:17:47 +0900

    Step10 에 MainClass01 추가

---
+++ Step10_Interface/src/test/main/MainClass01.java
@@ -0,0 +1,26 @@
+package test.main;
+
+import test.mypac.MyRemocon;
+import test.mypac.Remocon;
+
+public class MainClass01 {
+
+	public static void main(String[] args) {
+		/*
+		 * Remocon 인터페이스는 data type 의 역할을 할 수 있는데 
+		 * 해당 type 의 참조값을 얻어내기 위해서는 인터페이스를 구현한 클래스로
+		 * 객체 생성을 하면 된다.
+		 */ 
+//		추상 클래스(단일 상속)와 비슷이지만 인터페이스는 여러개를 상속할 수 있다는 점이 다르다
+		Remocon r1 = new MyRemocon();
+		r1.up();
+		r1.down();
+	}
+
+}
+
+//class 나 implements 공인중계사, 정보처리기사, 회계사{
+//	
+//}
+//
+//공인중계사 [] =  new 나();
--- Step10_Interface/src/test/mypac/MyRemocon.java
+++ Step10_Interface/src/test/mypac/MyRemocon.java
@@ -2,6 +2,7 @@ package test.mypac;
 	
 public class MyRemocon implements Remocon {// 인터페이스는 implements 해야 한다
 
+//	미완성인 메소드를 제정의override 강제
 	public void up() {
 		System.out.println("볼륨을 올려요!");
 	}
